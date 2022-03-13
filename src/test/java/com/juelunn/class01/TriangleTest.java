b56a324b1fd5613af7f623d9599bbd307487976d
绝伦N <1308964967@qq.com>
2022-03-13 13:58:57 +0800

    fix: test case

--- src/test/java/com/juelunn/class01/TriangleTest.java
+++ src/test/java/com/juelunn/class01/TriangleTest.java
@@ -24,43 +24,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * @description 三角形测试用例
  */
 class TriangleTest {
-
-    @Test
-    @DisplayName("输入错误")
-    void parameters_error_test() {
-        Triangle triangle = new Triangle();
-        String type = triangle.classify(0, 4, 5);
-        assertEquals("输入错误", type);
-    }
-    @Test
-    @DisplayName("非三角形")
-    void not_fit_test() {
-        Triangle triangle = new Triangle();
-        String type = triangle.classify(2, 4, 10);
-        assertEquals("非三角形", type);
-    }
-    @Test
-    @DisplayName("等边三角形")
-    void scale_test() {
-        Triangle triangle = new Triangle();
-        String type = triangle.classify(6, 6, 6);
-        assertEquals("等边三角形", type);
-    }
-    @Test
-    @DisplayName("等腰三角形")
-    void scalene_test() {
-        Triangle triangle = new Triangle();
-        String type = triangle.classify(4, 4, 6);
-        assertEquals("等腰三角形", type);
-    }
-    @Test
-    @DisplayName("不等边三角形")
-    void common_test() {
-        Triangle triangle = new Triangle();
-        String type = triangle.classify(3, 4, 5);
-        assertEquals("不等边三角形", type);
-    }
-
     @ParameterizedTest
     @DisplayName(value="覆盖性测试")
     @CsvSource({
@@ -77,6 +40,31 @@ class TriangleTest {
             "3,4,5,不等边三角形",
     })
     void paramTriangle(int a, int b,int c,String expected) {
+        Triangle triangle = new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+
+    @ParameterizedTest
+    @DisplayName(value="边界用例测试")
+    @CsvSource({
+            "1,50,50,等腰三角形",
+            "2,50,50,等腰三角形",
+            "99,50,50,等腰三角形",
+            "100,50,50,非三角形",
+            "50,1,50,等腰三角形",
+            "50,2,50,等腰三角形",
+            "50,99,50,等腰三角形",
+            "50,100,50,非三角形",
+            "50,50,1,等腰三角形",
+            "50,50,2,等腰三角形",
+            "50,50,99,等腰三角形",
+            "50,50,100,非三角形",
+            "50,50,50,等边三角形",
+    })
+    void paramTriangle1(int a, int b,int c,String expected) {
         Triangle triangle = new Triangle();
 
         String type = triangle.classify(a, b, c);
